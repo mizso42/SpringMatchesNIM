a078dc31f7e8c2e2e0532621e37c0086898990ad
mizso42 <jnzsmkls@gmail.com>
2021-10-09 14:25:17 +0200

    Initial commit

---
+++ src/main/java/org/progmatic/java/backend/SpringMatchesNIM/SpringMatchesNimApplication.java
@@ -0,0 +1,13 @@
+package org.progmatic.java.backend.SpringMatchesNIM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMatchesNimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMatchesNimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/progmatic/java/backend/SpringMatchesNIM/SpringMatchesNimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.progmatic.java.backend.SpringMatchesNIM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMatchesNimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
